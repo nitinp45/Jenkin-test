0f6db21cabc76d196cf68cfd997cc5ffd04e5611
Nitin Patil <92635975+nitinp45@users.noreply.github.com>
2024-05-13 09:16:18 +0530

    Add files via upload

---
+++ FirstJob.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class FirstJob
+{
+    public static void main(String[] args) {
+        System.out.print("This is my First Job");
+        System.out.println(new Date());
+    }
+}
